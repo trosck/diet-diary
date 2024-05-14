f377f0a16f9c60564cc7e589568fa38d5eda8e65
Sergey Belov <kort.dll@gmail.com>
2024-05-14 04:07:46 +0300

    remove pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/i18n", "@nuxt/ui", "@pinia/nuxt"],
+  modules: ["@nuxtjs/i18n", "@nuxt/ui"],
   i18n: {
     strategy: "no_prefix",
     defaultLocale: "ru",
