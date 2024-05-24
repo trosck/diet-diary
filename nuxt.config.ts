ad975a42785bce42e38099f703eff3fcc27a78bf
Sergey Belov <kort.dll@gmail.com>
2024-05-24 17:15:32 +0300

    add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/i18n", "@nuxt/ui"],
+  modules: ["@nuxtjs/i18n", "@nuxt/ui", "@pinia/nuxt"],
   i18n: {
     strategy: "no_prefix",
     defaultLocale: "ru",
