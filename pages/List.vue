846bbdb0eb12f5ef6e8868425f307326a9bdfb02
Sergey Belov <kort.dll@gmail.com>
2024-05-25 17:16:50 +0300

    add products list

--- pages/List.vue
+++ pages/List.vue
@@ -1,3 +1,33 @@
-<template>some</template>
+<template>
+  <div class="flex">
+    <UInput
+      :ui="{ wrapper: 'grow' }"
+      icon="i-heroicons-magnifying-glass-20-solid"
+      :trailing="false"
+      :placeholder="$t('search') + '...'"
+      v-model="search"
+      color="white"
+    />
 
-<script setup lang="ts"></script>
+    <UButton icon="i-heroicons-plus" :ui="{ base: 'ml-3' }" square />
+  </div>
+
+  <ProductCard
+    v-for="product of filteredProducts"
+    v-bind="product"
+    class="mt-5"
+  />
+</template>
+
+<script setup lang="ts">
+const search = ref("");
+
+const productsStore = useProductsStore();
+
+const filteredProducts = computed(() => {
+  const searchValue = search.value.toLowerCase();
+  return productsStore.products.filter((product) =>
+    product.name.toLowerCase().includes(searchValue)
+  );
+});
+</script>
