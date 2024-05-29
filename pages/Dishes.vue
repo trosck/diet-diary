938e886d1a7a0b89a977316c16cb408a2f3bf02a
Sergey Belov <kort.dll@gmail.com>
2024-05-30 00:19:03 +0300

    use dishes store

--- pages/Dishes.vue
+++ pages/Dishes.vue
@@ -21,15 +21,44 @@
     />
   </UButtonGroup>
 
-  <!-- <ProductCard
-    v-for="product of filteredProducts"
-    v-bind="product"
-    class="mt-5"
-  /> -->
+  <div v-for="dish of filteredDishes">
+    <ProductCard
+      v-bind="calculateMealNutritions(dish)"
+      :name="dish.name"
+      class="mt-5"
+    />
+
+    <UButtonGroup>
+      <UButton
+        icon="i-heroicons-edit"
+        square
+        color="gray"
+        @click="isModalOpen = true"
+      />
+    </UButtonGroup>
+  </div>
+
+  <AddDishModal v-model="isModalOpen" @save="addDish" />
 </template>
 
 <script setup lang="ts">
+import { calculateMealNutritions } from "~/lib/calc";
+import type { Dish } from "~/types/Dish";
+
 const search = ref("");
 
 const isModalOpen = ref(false);
+
+const dishesStore = useDishesStore();
+
+function addDish(dish: Dish) {
+  dishesStore.addDish(dish);
+}
+
+const filteredDishes = computed(() => {
+  const searchQuery = search.value.toLowerCase();
+  return dishesStore.dishes.filter((dish) =>
+    dish.name.toLowerCase().includes(searchQuery)
+  );
+});
 </script>
