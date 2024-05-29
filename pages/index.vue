7cc71dde330768501c35cce706af38d665a3127e
Sergey Belov <kort.dll@gmail.com>
2024-05-29 22:08:04 +0300

    rename meals to dishes

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
       </div>
 
       <div v-show="screen === SCREEN_TYPE.DIARY"><Diary /></div>
-      <div v-show="screen === SCREEN_TYPE.LIST"><Meals /></div>
+      <div v-show="screen === SCREEN_TYPE.DISHES"><Dishes /></div>
     </div>
 
     <div class="flex justify-between w-full">
@@ -28,15 +28,15 @@
 
 <script setup lang="ts">
 import Diary from "./Diary.vue";
-import Meals from "./Meals.vue";
+import Dishes from "./Dishes.vue";
 
 const SCREEN_TYPE = {
   DIARY: "diary",
-  LIST: "meals",
+  DISHES: "dishes",
   SOME: "some",
 };
 
-const screen = ref(SCREEN_TYPE.LIST);
+const screen = ref(SCREEN_TYPE.DISHES);
 
 const isLoading = ref(false);
 
