0e7073d8c75f5ef660195e700de6e47aac856546
Sergey Belov <kort.dll@gmail.com>
2024-05-29 19:26:43 +0300

    refactoring select menu

--- components/ProductSearch.vue
+++ components/ProductSearch.vue
@@ -1,9 +1,21 @@
 <template>
   <USelectMenu
+    :ui="{ base: 'u-select-menu' }"
     searchable
     searchable-placeholder="Search a product..."
     placeholder="Select a product"
-    :options="['Wade Cooper', 'Arlene Mccoy', 'Devon Webb', 'Tom Cook', 'Tanya Fox', 'Hellen Schmidt', 'Caroline Schultz', 'Mason Heaney', 'Claudie Smitham', 'Emil Schaefer']"
+    :options="[
+      'Wade Cooper',
+      'Arlene Mccoy',
+      'Devon Webb',
+      'Tom Cook',
+      'Tanya Fox',
+      'Hellen Schmidt',
+      'Caroline Schultz',
+      'Mason Heaney',
+      'Claudie Smitham',
+      'Emil Schaefer',
+    ]"
     model-value="Wade Cooper"
   />
 </template>
