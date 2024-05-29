5be9514b6088c69e80be19c5814e6a82107abd41
Sergey Belov <kort.dll@gmail.com>
2024-05-29 14:59:21 +0300

    refactoring layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,26 @@
 <template>
-  <div class="w-full relative h-screen">
-    <div class="p-5">
+  <div class="w-full relative h-screen flex flex-col">
+    <div class="p-5 overflow-auto grow">
       <div class="fixed z-10" v-if="isLoading">
         <USkeleton class="h-40 w-full mb-5" />
         <USkeleton class="h-40 w-full mb-5" />
         <USkeleton class="h-40 w-full" />
       </div>
 
-      <Diary v-if="screen === SCREEN_TYPE.DIARY" />
-      <List v-if="screen === SCREEN_TYPE.LIST" />
+      <div v-show="screen === SCREEN_TYPE.DIARY"><Diary /></div>
+      <div v-show="screen === SCREEN_TYPE.LIST"><List /></div>
     </div>
 
-    <div class="absolute bottom-0 flex justify-between w-full h-14">
+    <div class="flex justify-between w-full">
       <UButton
-        :label="$t('diary')"
-        @click="screen = SCREEN_TYPE.DIARY"
-        :variant="screen === SCREEN_TYPE.DIARY ? 'outline' : 'solid'"
-        :ui="{ base: 'grow flex justify-center', rounded: 'rounded-none' }"
-      />
-      <UButton
-        :label="$t('list')"
-        @click="screen = SCREEN_TYPE.LIST"
-        :variant="screen === SCREEN_TYPE.LIST ? 'outline' : 'solid'"
-        :ui="{ base: 'grow flex justify-center', rounded: 'rounded-none' }"
-      />
-      <UButton
-        label="some"
-        @click="screen = SCREEN_TYPE.SOME"
-        :variant="screen === SCREEN_TYPE.SOME ? 'outline' : 'solid'"
-        :ui="{ base: 'grow flex justify-center', rounded: 'rounded-none' }"
+        v-for="buttonName of Object.values(SCREEN_TYPE)"
+        :label="$t(buttonName)"
+        @click="screen = buttonName"
+        :variant="screen === buttonName ? 'soft' : 'solid'"
+        :ui="{
+          base: 'grow flex justify-center capitalize',
+          rounded: 'rounded-none',
+        }"
       />
     </div>
   </div>
@@ -44,7 +36,7 @@ const SCREEN_TYPE = {
   SOME: "some",
 };
 
-const screen = ref(SCREEN_TYPE.LIST);
+const screen = ref(SCREEN_TYPE.DIARY);
 
 const isLoading = ref(false);
 
