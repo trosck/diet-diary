dcb3eb28a3eece14786a24e31dd91c7e3d89770c
Sergey Belov <kort.dll@gmail.com>
2024-05-29 23:59:52 +0300

    ref, add unmounted

--- plugins/select-on-click-directive.ts
+++ plugins/select-on-click-directive.ts
@@ -1,9 +1,14 @@
+function select(event: any) {
+  event.target.select();
+}
+
 export default defineNuxtPlugin((app) => {
   app.vueApp.directive("select-on-click", {
     mounted(el) {
-      el.addEventListener("click", (event: any) => {
-        event.target.select();
-      });
+      el.addEventListener("click", select);
+    },
+    unmounted(el) {
+      el.removeEventListener("click", select);
     },
   });
 });
