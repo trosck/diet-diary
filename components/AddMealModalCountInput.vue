f4c0e5c19c7fe1013ebe1d1d30d99c3864c9457a
Sergey Belov <kort.dll@gmail.com>
2024-05-29 18:35:09 +0300

    add AddMealModalCountInput

---
+++ components/AddMealModalCountInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <UButtonGroup v-if="model">
+    <UInput
+      icon="i-heroicons-scale"
+      v-model="model[measurementKey]"
+      @click="(e: any) => e.target.select()"
+    />
+
+    <USelect
+      v-model="measurementType"
+      :options="[
+        $t('measurement.quantity.short'),
+        $t('measurement.gramm.short'),
+      ]"
+    />
+  </UButtonGroup>
+</template>
+
+<script setup lang="ts">
+import type { ProductsListItem } from "~/types/Meal";
+
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
+
+const model = defineModel<ProductsListItem>();
+
+const measurementType = ref(t("measurement.gramm.short"));
+const measurementKey = computed(() =>
+  measurementType.value === t("measurement.gramm.short") ? "weight" : "amount"
+);
+</script>
