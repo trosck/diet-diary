894f7e0931dbf72927eb88d3bf5e02a81c259e02
Sergey Belov <kort.dll@gmail.com>
2024-05-29 19:26:35 +0300

    disable devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: ["@nuxtjs/i18n", "@nuxt/ui", "@pinia/nuxt"],
   i18n: {
     strategy: "no_prefix",
