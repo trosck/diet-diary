53b952edbfbb456517a62d5c0a09266e6a5c56cb
Sergey Belov <kort.dll@gmail.com>
2024-05-29 16:39:37 +0300

    refactoring, add list of meals

--- pages/Diary.vue
+++ pages/Diary.vue
@@ -1,61 +1,87 @@
 <template>
-  <UPopover
-    :popper="{ placement: 'bottom-start' }"
-    :ui="{ wrapper: 'mb-5 w-full' }"
+  <UButtonGroup
+    orientation="horizontal"
+    :ui="{ wrapper: { horizontal: 'flex sticky top-0' } }"
   >
+    <UPopover
+      :popper="{ placement: 'bottom-start' }"
+      :ui="{ wrapper: 'w-full sticky top-0' }"
+    >
+      <UButton
+        icon="i-heroicons-calendar-days-20-solid"
+        :label="format(date, 'd MMM, yyy')"
+        block
+      />
+
+      <template #panel="{ close }">
+        <DatePicker v-model="date" is-required @close="close" />
+      </template>
+    </UPopover>
+
     <UButton
-      icon="i-heroicons-calendar-days-20-solid"
-      :label="format(date, 'd MMM, yyy')"
-      block
+      icon="i-heroicons-plus"
+      :ui="{ base: 'ml-3' }"
+      square
+      color="gray"
+      @click="isModalOpen = true"
     />
+  </UButtonGroup>
 
-    <template #panel="{ close }">
-      <DatePicker v-model="date" is-required @close="close" />
-    </template>
-  </UPopover>
+  <div v-for="meal of diaryStore.meals">
+    <ProductCard
+      v-bind="calculateMealNutritions(meal)"
+      :name="meal.name"
+      class="mt-5"
+    />
+    <UButtonGroup>
+      <UButton
+        icon="i-heroicons-edit"
+        square
+        color="gray"
+        @click="isModalOpen = true"
+      />
+    </UButtonGroup>
+  </div>
 
-  <ProductCard
-    v-for="product of productsStore.products"
-    v-bind="product"
-    class="mb-5"
-  />
-
-  <UButton
-    :label="$t('add')"
-    icon="i-heroicons-plus-16-solid"
-    class="m-auto"
-    @click="isModalOpen = true"
-  />
-
-  <UModal v-model="isModalOpen">
-    <UCard>
-      <UForm>
-        <UFormGroup label="Product" name="product_id">
-          <ProductSelectMenu />
-        </UFormGroup>
-      </UForm>
-    </UCard>
-  </UModal>
+  <AddMealModal v-model="isModalOpen" @save="addMeal" />
 </template>
 
 <script setup lang="ts">
 import ProductCard from "~/components/ProductCard.vue";
-import ProductSelectMenu from "~/components/ProductSelectMenu.vue";
 import DatePicker from "~/components/DatePicker.vue";
 
 import { format } from "date-fns";
+import type { Meal, ProductsListItem } from "~/types/Meal";
+import type { Product } from "~/types/Product";
 
 const date = ref(new Date());
 
-const productsStore = useProductsStore();
+const diaryStore = useDiaryStore();
 
 const isModalOpen = ref(false);
 
-const modalState = reactive({
-  name: undefined,
-  calories: undefined,
-  fats: undefined,
-  proteins: undefined,
-  carbs: undefined,
-});
+function calculateMealNutritions(meal: Meal): Product {
+  const nutrients = {
+    calories: 0,
+    fats: 0,
+    carbs: 0,
+    proteins: 0,
+  };
+
+  for (const product of meal.products) {
+    for (const nutrientKey in nutrients) {
+      const key = nutrientKey as keyof typeof nutrients;
+      nutrients[key] += +(product[key] * (product.weight / 1000)).toFixed(1);
+    }
+  }
+
+  return {
+    name: meal.name,
+    ...nutrients,
+  };
+}
+
+function addMeal(meal: Meal) {
+  diaryStore.addMeal(meal);
+}
 </script>
