ff86c8b8c2ab5f85d95f62d391a03ae4d858d5cb
Sergey Belov <kort.dll@gmail.com>
2024-05-31 03:03:39 +0300

    add indexeddb helper

---
+++ composables/useIndexedDB.ts
@@ -0,0 +1,51 @@
+import { openDB, type IDBPDatabase, type IDBPObjectStore } from "idb";
+
+const db_name = "dietDiary";
+const db_version = 2;
+
+let db: IDBPDatabase;
+async function useIndexedDB() {
+  if (db) return db;
+
+  db = await openDB(db_name, db_version, {
+    upgrade(database, oldVersion, newVersion, transaction, event) {
+      database.createObjectStore("products", {
+        autoIncrement: true,
+        keyPath: "id",
+      });
+
+      database.createObjectStore("dishes", {
+        autoIncrement: true,
+      });
+
+      const diaryStore = database.createObjectStore("diary", {
+        autoIncrement: true,
+      });
+
+      diaryStore.createIndex("date", "date");
+    },
+  });
+
+  return db;
+}
+
+export function useObjectStore(storeName: string) {
+  return async function (
+    callback: (
+      store: IDBPObjectStore<
+        unknown,
+        ArrayLike<string>,
+        string,
+        "readonly" | "readwrite"
+      >
+    ) => Promise<any> | any,
+    mode: IDBTransactionMode = "readonly"
+  ) {
+    const db = await useIndexedDB();
+    const transaction = db.transaction(storeName, mode);
+    const store = transaction.objectStore(storeName);
+    const result = await callback(store);
+    await transaction.done;
+    return result;
+  };
+}
