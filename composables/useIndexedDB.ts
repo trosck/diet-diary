69a13f6d438c0fc8efa1fc6f19a01e5219e4ebaf
Sergey Belov <kort.dll@gmail.com>
2024-05-31 19:04:16 +0300

    refactoring useIndexedDB

--- composables/useIndexedDB.ts
+++ composables/useIndexedDB.ts
@@ -1,9 +1,11 @@
 import {
+  deleteDB,
   openDB,
   type DBSchema,
   type IDBPDatabase,
   type IDBPObjectStore,
 } from "idb";
+
 import type { Dish } from "~/types/Dish";
 import type { Meal } from "~/types/Meal";
 import type { Product } from "~/types/Product";
@@ -24,51 +26,25 @@ interface Schema extends DBSchema {
   };
 }
 
-type StoreName = "products" | "dishes" | "diary";
-
 const db_name = "dietDiary";
 const db_version = 1;
 
-let db: IDBPDatabase<Schema>;
-async function useIndexedDB() {
-  if (db) return db;
-
-  db = await openDB<Schema>(db_name, db_version, {
+export async function useIndexedDB() {
+  return openDB<Schema>(db_name, db_version, {
     upgrade(database, oldVersion, newVersion, transaction, event) {
       database.createObjectStore("products", {
-        autoIncrement: true,
         keyPath: "id",
       });
 
       database.createObjectStore("dishes", {
-        autoIncrement: true,
         keyPath: "id",
       });
 
       database
         .createObjectStore("diary", {
-          autoIncrement: true,
           keyPath: "id",
         })
         .createIndex("date", "date");
     },
   });
-
-  return db;
-}
-
-export function useObjectStore(storeName: StoreName) {
-  return async function (
-    callback: (
-      store: IDBPObjectStore<Schema, [StoreName], StoreName, IDBTransactionMode>
-    ) => Promise<any> | any,
-    mode: IDBTransactionMode = "readonly"
-  ) {
-    const db = await useIndexedDB();
-    const transaction = db.transaction(storeName, mode);
-    const store = transaction.objectStore(storeName);
-    const result = await callback(store);
-    await transaction.done;
-    return result;
-  };
 }
