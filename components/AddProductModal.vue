88c39c9e099dd3221571e0be5d5d509d1d1529f5
Sergey Belov <kort.dll@gmail.com>
2024-06-03 04:01:27 +0300

    refactoring

--- components/AddProductModal.vue
+++ components/AddProductModal.vue
@@ -2,14 +2,14 @@
   <UModal v-model="model" :ui="{ container: 'flex-col', base: 'grow' }">
     <UForm class="flex flex-col p-5 grow" :state="product">
       <div class="grow">
-        <UFormGroup :label="$t('name')" :ui="{ wrapper: 'mb-5' }">
+        <UFormGroup :label="$t('name')" :ui="{ wrapper: 'mb-5 capitalize' }">
           <UInput v-model="product.name" />
         </UFormGroup>
 
         <UFormGroup
           v-for="fieldName of formFields"
           :label="$t(fieldName)"
-          :ui="{ wrapper: 'mb-5' }"
+          :ui="{ wrapper: 'mb-5 capitalize' }"
         >
           <UInput v-model="product[fieldName]" v-select-on-click />
         </UFormGroup>
@@ -22,18 +22,14 @@
           @click="cancel"
           :label="$t('cancel')"
         />
-        <UButton
-          :ui="{ base: 'grow' }"
-          @click="save"
-          :label="$t('add')"
-        />
+        <UButton :ui="{ base: 'grow' }" @click="save" :label="$t('add')" />
       </UButtonGroup>
     </UForm>
   </UModal>
 </template>
 
 <script setup lang="ts">
-import type { Product } from "~/types/Product";
+import type { Nutrients, Product } from "~/types/Product";
 
 const emit = defineEmits<{
   (e: "save", value: Product): void;
@@ -41,29 +37,29 @@ const emit = defineEmits<{
 
 const model = defineModel();
 
-const formFields: Partial<keyof Product>[] = [
+const formFields: (keyof Nutrients)[] = [
   "calories",
   "carbs",
   "fats",
   "proteins",
 ];
 
-const baseItem = {
+const getBaseItem = () => ({
   name: "",
   calories: 0,
   proteins: 0,
   fats: 0,
   carbs: 0,
-};
+});
 
-const product = reactive({ ...baseItem });
+const product = ref(getBaseItem());
 
 function closeModal() {
   model.value = false;
 }
 
 function clearState() {
-  Object.assign(product, baseItem);
+  product.value = getBaseItem();
 }
 
 function cancel() {
@@ -72,7 +68,7 @@ function cancel() {
 }
 
 function save() {
-  emit("save", toRaw(product));
+  emit("save", product.value);
   cancel();
 }
 </script>
