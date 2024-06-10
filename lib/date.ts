bb6d09799f005108e2fcd7803a87e1ecdba2e2f0
Sergey Belov <kort.dll@gmail.com>
2024-06-10 05:32:05 +0300

    add getFormattedDate and getFormattedTime

--- lib/date.ts
+++ lib/date.ts
@@ -1,6 +1,16 @@
+import { format } from "date-fns";
+
 export function resetTime(date: Date) {
   date.setMilliseconds(0);
   date.setSeconds(0);
   date.setMinutes(0);
   date.setHours(0);
+}
+
+export function getFormattedDate(date: Date) {
+  return format(date, "dd.MM.yyyy");
+}
+
+export function getFormattedTime(date: Date) {
+  return format(date, "kk:mm:ss");
 }
